76a80bb7d4e92cddb8cbac9901cb52c42a88fdbf
ねんねこ <95109758+nennneko5787@users.noreply.github.com>
2026-05-17 21:42:05 +0900

    layout.tsx を更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Twitfiend Saga",
   description:
-    "Enter the world of Twitfiend Saga—where obsession never ends. Read for free with unlimited access and lose yourself in the endless cycle of tweets and madness.",
+    "Enter the world of ツイ廃廻戦[Twitfiend Saga]—where obsession never ends. Read for free with unlimited access and lose yourself in the endless cycle of tweets and madness.",
 };
 
 export default function RootLayout({
